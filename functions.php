b561e758c551a8571ef89f3b05e7558305b9956e
Codix Produtora Digital <65787087+codixdigital@users.noreply.github.com>
2020-06-11 07:37:21 -0300

    Delete functions.php

--- functions.php
+++
@@ -1,6 +0,0 @@
-<?php // for correct function of the code, the functions.php file can only have one code opening "<? php"
-
-// For correct function function execution, add this code in the last line of the functions.php file
-$codix_content = get_the_content (); echo mb_strimwidth($codix_content, 0, 400, '[...]');
-
-// for correct code function, the functions.php file can only have a "? >" code closure ?>
