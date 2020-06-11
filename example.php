1f5b2adf16fa7f61b505e5e0c4dc5bdef22667c0
Codix Produtora Digital <65787087+codixdigital@users.noreply.github.com>
2020-06-11 07:40:45 -0300

    Update example.php

--- example.php
+++ example.php
@@ -1,5 +1,8 @@
-<?php 
-// This code does not need to be called via the functions.php file
+<?php // This code does not need to be called via the functions.php file
 
+// getting post content
+$codix_content = get_the_content ();
 
+// printing content with a character limit
+echo mb_strimwidth($codix_content, 0, 400, '[...]');
 ?>
